f85127637754e94f5f2c11ff1177aa5e4b4f18ef
eugene <eugene@levinriegner.com>
2016-12-15 15:52:27 +0300

    added helpers

---
+++ helpers.js
@@ -0,0 +1,52 @@
+function setupGui() {
+
+	var h;
+
+	var gui = new dat.GUI();
+
+	// material (attributes)
+
+	h = gui.addFolder( "Material control" );
+
+	h.add( effectController, "shininess", 1.0, 400.0, 1.0 ).name( "shininess" ).onChange( render );
+	h.add( effectController, "kd", 0.0, 1.0, 0.025 ).name( "diffuse strength" ).onChange( render );
+	h.add( effectController, "ks", 0.0, 1.0, 0.025 ).name( "specular strength" ).onChange( render );
+	h.add( effectController, "metallic" ).onChange( render );
+
+	// material (color)
+
+	h = gui.addFolder( "Material color" );
+
+	h.add( effectController, "hue", 0.0, 1.0, 0.025 ).name( "hue" ).onChange( render );
+	h.add( effectController, "saturation", 0.0, 1.0, 0.025 ).name( "saturation" ).onChange( render );
+	h.add( effectController, "lightness", 0.0, 1.0, 0.025 ).name( "lightness" ).onChange( render );
+
+	// light (point)
+
+	h = gui.addFolder( "Lighting" );
+
+	h.add( effectController, "lhue", 0.0, 1.0, 0.025 ).name( "hue" ).onChange( render );
+	h.add( effectController, "lsaturation", 0.0, 1.0, 0.025 ).name( "saturation" ).onChange( render );
+	h.add( effectController, "llightness", 0.0, 1.0, 0.025 ).name( "lightness" ).onChange( render );
+	h.add( effectController, "ka", 0.0, 1.0, 0.025 ).name( "ambient" ).onChange( render );
+
+	// light (directional)
+
+	h = gui.addFolder( "Light direction" );
+
+	h.add( effectController, "lx", -1.0, 1.0, 0.025 ).name( "x" ).onChange( render );
+	h.add( effectController, "ly", -1.0, 1.0, 0.025 ).name( "y" ).onChange( render );
+	h.add( effectController, "lz", -1.0, 1.0, 0.025 ).name( "z" ).onChange( render );
+
+	h = gui.addFolder( "Tessellation control" );
+	h.add( effectController, "newTess", [ 2, 3, 4, 5, 6, 8, 10, 15, 20, 30, 40, 50 ] ).name( "Tessellation Level" ).onChange( render );
+	h.add( effectController, "lid" ).name( "display lid" ).onChange( render );
+	h.add( effectController, "body" ).name( "display body" ).onChange( render );
+	h.add( effectController, "bottom" ).name( "display bottom" ).onChange( render );
+	h.add( effectController, "fitLid" ).name( "snug lid" ).onChange( render );
+	h.add( effectController, "nonblinn" ).name( "original scale" ).onChange( render );
+
+	// shading
+	h = gui.add( effectController, "newShading", [ "wireframe", "flat", "smooth", "glossy", "textured", "reflective" ] ).name( "Shading" ).onChange( render );
+
+}
--- main.js
+++ main.js
@@ -1,4 +1,4 @@
-// Boilerplate example:
+// Boilerplate example based on:
 // https://github.com/mrdoob/three.js/blob/master/examples/webgl_geometry_teapot.html
 // Utah/Newell Teapot demo
 
@@ -7,7 +7,36 @@ var examplepath = 'https://threejs.org/examples/';
 
 var camera, scene, renderer;
 var cameraControls;
-var effectController;
+
+var effectController = {
+  shininess: 40.0,
+  ka: 0.17,
+  kd: 0.51,
+  ks: 0.2,
+  metallic: true,
+
+  hue:		0.121,
+  saturation: 0.73,
+  lightness:  0.66,
+
+  lhue:		 0.04,
+  lsaturation: 0.01,	// non-zero so that fractions will be shown
+  llightness:  1.0,
+
+  // bizarrely, if you initialize these with negative numbers, the sliders
+  // will not show any decimal places.
+  lx: 0.32,
+  ly: 0.39,
+  lz: 0.7,
+  newTess: 15,
+  bottom: true,
+  lid: true,
+  body: true,
+  fitLid: false,
+  nonblinn: false,
+  newShading: "glossy"
+};
+
 var teapotSize = 400;
 var ambientLight, light;
 var skybox;
@@ -102,91 +131,6 @@ function init() {
 
 }
 
-function setupGui() {
-
-	effectController = {
-
-		shininess: 40.0,
-		ka: 0.17,
-		kd: 0.51,
-		ks: 0.2,
-		metallic: true,
-
-		hue:		0.121,
-		saturation: 0.73,
-		lightness:  0.66,
-
-		lhue:		 0.04,
-		lsaturation: 0.01,	// non-zero so that fractions will be shown
-		llightness:  1.0,
-
-		// bizarrely, if you initialize these with negative numbers, the sliders
-		// will not show any decimal places.
-		lx: 0.32,
-		ly: 0.39,
-		lz: 0.7,
-		newTess: 15,
-		bottom: true,
-		lid: true,
-		body: true,
-		fitLid: false,
-		nonblinn: false,
-		newShading: "glossy"
-	};
-
-	var h;
-
-	var gui = new dat.GUI();
-
-	// material (attributes)
-
-	h = gui.addFolder( "Material control" );
-
-	h.add( effectController, "shininess", 1.0, 400.0, 1.0 ).name( "shininess" ).onChange( render );
-	h.add( effectController, "kd", 0.0, 1.0, 0.025 ).name( "diffuse strength" ).onChange( render );
-	h.add( effectController, "ks", 0.0, 1.0, 0.025 ).name( "specular strength" ).onChange( render );
-	h.add( effectController, "metallic" ).onChange( render );
-
-	// material (color)
-
-	h = gui.addFolder( "Material color" );
-
-	h.add( effectController, "hue", 0.0, 1.0, 0.025 ).name( "hue" ).onChange( render );
-	h.add( effectController, "saturation", 0.0, 1.0, 0.025 ).name( "saturation" ).onChange( render );
-	h.add( effectController, "lightness", 0.0, 1.0, 0.025 ).name( "lightness" ).onChange( render );
-
-	// light (point)
-
-	h = gui.addFolder( "Lighting" );
-
-	h.add( effectController, "lhue", 0.0, 1.0, 0.025 ).name( "hue" ).onChange( render );
-	h.add( effectController, "lsaturation", 0.0, 1.0, 0.025 ).name( "saturation" ).onChange( render );
-	h.add( effectController, "llightness", 0.0, 1.0, 0.025 ).name( "lightness" ).onChange( render );
-	h.add( effectController, "ka", 0.0, 1.0, 0.025 ).name( "ambient" ).onChange( render );
-
-	// light (directional)
-
-	h = gui.addFolder( "Light direction" );
-
-	h.add( effectController, "lx", -1.0, 1.0, 0.025 ).name( "x" ).onChange( render );
-	h.add( effectController, "ly", -1.0, 1.0, 0.025 ).name( "y" ).onChange( render );
-	h.add( effectController, "lz", -1.0, 1.0, 0.025 ).name( "z" ).onChange( render );
-
-	h = gui.addFolder( "Tessellation control" );
-	h.add( effectController, "newTess", [ 2, 3, 4, 5, 6, 8, 10, 15, 20, 30, 40, 50 ] ).name( "Tessellation Level" ).onChange( render );
-	h.add( effectController, "lid" ).name( "display lid" ).onChange( render );
-	h.add( effectController, "body" ).name( "display body" ).onChange( render );
-	h.add( effectController, "bottom" ).name( "display bottom" ).onChange( render );
-	h.add( effectController, "fitLid" ).name( "snug lid" ).onChange( render );
-	h.add( effectController, "nonblinn" ).name( "original scale" ).onChange( render );
-
-	// shading
-	h = gui.add( effectController, "newShading", [ "wireframe", "flat", "smooth", "glossy", "textured", "reflective" ] ).name( "Shading" ).onChange( render );
-
-}
-
-
-
 function render() {
 
 	if ( effectController.newTess !== tess ||
@@ -197,7 +141,7 @@ function render() {
 		effectController.nonblinn !== bNonBlinn ||
 		effectController.newShading !== shading )
 	{
-
+  
 		tess = effectController.newTess;
 		bBottom = effectController.bottom;
 		bLid = effectController.lid;
@@ -205,9 +149,9 @@ function render() {
 		bFitLid = effectController.fitLid;
 		bNonBlinn = effectController.nonblinn;
 		shading = effectController.newShading;
-
+  
 		createNewTeapot();
-
+  
 	}
 
 	// We're a bit lazy here. We could check to see if any material attributes changed and update
